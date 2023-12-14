f95c2169e452993bdf0d4c2c3e0592d833dc123f
inoh <syslink.h.inoue@gmail.com>
2023-12-14 19:43:19 +0900

    Add user crud sample

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,53 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { UserService } from './user.service';
 import { AppService } from './app.service';
+import { User as UserModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('users')
+  async getUsers(): Promise<UserModel[]> {
+    return this.userService.users();
+  }
+
+  @Post('users')
+  async craeteUser(
+    @Body() userData: { name: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+
+  @Put('users/:id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() userData: { name: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: userData,
+    });
+  }
+
+  @Delete('users/:id')
+  async deletePost(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id: Number(id) });
   }
 }
