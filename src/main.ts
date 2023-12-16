d6bef515fc533245518edf415a8009b3e1750d1a
inoh <syslink.h.inoue@gmail.com>
2023-12-16 12:31:28 +0900

    ベースを Fastify に変更

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    // アプリケーション作成に失敗した場合は例外を投げる
-    abortOnError: false,
-  });
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
